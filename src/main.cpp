cc6717c1e95d7eaf1aab966d65ed3a989970502c
Alex Toussaint <belzebalex@users.noreply.github.com>
2018-12-02 22:12:39 +0100

    commentaires

--- src/main.cpp
+++ src/main.cpp
@@ -19,28 +19,29 @@ int main() {
     vector<Vector2f> sources;
 
     Json::Value root;
-    ifstream file("parameters.json");
+    ifstream file("parameters.json");  // On vient lire les paramètres dans le fichier JSON
     file >> root;
 
     unsigned int wavelength          = root["wavelength"].asFloat();
     double delta_shift_per_iteration = root["delta_shift_per_iteration"].asFloat();
 
     for(unsigned int i = 0 ; i < root["sources"].size() ; i++)
-    {
-        sources.push_back(Vector2f(root["sources"][i]["x"].asFloat(), root["sources"][i]["y"].asFloat()));
+    {  // On ajoute les coordonnées dessources trouvées dans le fichier JSON
+        sources.push_back(Vector2f(root["sources"][i]["x"].asFloat(), root["sources"][i]["y"].asFloat()));  
     }
 
     sf::Vector2f window_size(1000, 1000);
     sf::Vector2f matrix_size = Vector2f(1000, 1000);
 
-    NodeMatrix matrix(matrix_size, root["compute_quantum"].asFloat());
+    NodeMatrix matrix(matrix_size, root["compute_quantum"].asFloat()); // La on déclare la matrice de "noeuds" de calcul
 
 
     sf::RenderWindow window(sf::VideoMode(window_size.x, window_size.y), "Wave Sim");
-    double phase_shift_per_unit = -3.14;
+    double phase_shift_per_unit = -3.14;   
+    // Cette variable contient le décalage de phase entre chaque source, c'est la seule qui change entre les diférentes images
     while (window.isOpen())
     {
-        phase_shift_per_unit += delta_shift_per_iteration;
+        phase_shift_per_unit += delta_shift_per_iteration;  // On lui ajoute sa dérivée, récupérée dans le fichier JSON
         cout << "phase: " << phase_shift_per_unit << "radians" << endl;
         sf::Event event;
         while (window.pollEvent(event))
@@ -50,17 +51,19 @@ int main() {
         }
 
         window.clear(sf::Color::Black);
-        matrix.reset();
+        matrix.reset();     // On remet à zero la matrice
 
 
-        for(unsigned int i = 0 ; i < sources.size() ; i++)
+        for(unsigned int i = 0 ; i < sources.size() ; i++)  
+            // Pour chaque source, on vient recalculer la matrice en lui donnant les coordonnées, la longueur d'onde et le décalage de phase
             matrix.computeMatrix(WaveSource(sources[i], wavelength, i * phase_shift_per_unit));
 
 
-        matrix.computeImage();
+        matrix.computeImage(); // On calcule l'image en fonction de la matrice
+
 
         window.draw(matrix);
-        for(unsigned int i = 0 ; i < sources.size() ; i++)
+        for(unsigned int i = 0 ; i < sources.size() ; i++)   // Pour chaque source, on dessine un cercle vert 
         {
             sf::CircleShape shape(5.f);
             shape.setFillColor(sf::Color(0, 255, 0));
